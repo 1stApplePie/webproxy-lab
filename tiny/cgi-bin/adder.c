a3a6d51cb64049f1f77e1e1d137ec8aaa770ed86
1stApplePie <chanwoo6422@naver.com>
2023-11-20 14:24:19 +0900

    implement: adder.c

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -2,9 +2,27 @@
  * adder.c - a minimal CGI program that adds two numbers together
  */
 /* $begin adder */
-#include "csapp.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  exit(0);
+    char *query_string = getenv("QUERY_STRING");
+
+    if (query_string == NULL) {
+        fprintf(stderr, "Error: No query string\n");
+        return 1;
+    }
+
+    int a, b;
+    // query_string에서 a와 b의 값을 읽어옴
+    if (sscanf(query_string, "a=%d&b=%d", &a, &b) != 2) {
+        fprintf(stderr, "Error: Invalid input\n");
+        return 1;
+    }
+
+    printf("Content-Type: text/plain\n\n"); // 헤더 출력
+    printf("Sum: %d\n", a + b);
+
+    return 0;
 }
 /* $end adder */
